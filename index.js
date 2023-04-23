99676c9e59746094e3f657642430a3e058d420e1
Maureen <morynmoh@gmail.com>
2023-04-23 14:08:18 +0300

    Completed Assignment on Palindrome

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+
+    word = word.toLowerCase();
+    
+      if (word === word.split('').reverse().join('')){
+        return true;
+      }  else {
+        return false;
+      }
+  }
+  
+  console.log(isPalindrome('racecar'));
+  console.log(isPalindrome('Book'));
+
 
 /* 
   Add your pseudocode here
+  Convert the string to lowercase
+  Reverse the string and compare with the original
 */
 
 /*
   Add written explanation of your solution here
+
+  The code takes the word and converts it to lower case using toLowerCase()
+  the word is then converted into an array of characters using split()
+  the order of the word is reversed using the method reverse()
+  The word characters are then joined back using the method join()
+
+  we compare the two words using if statement if true or false. 
 */
 
 // You can run `node index.js` to view these console logs
